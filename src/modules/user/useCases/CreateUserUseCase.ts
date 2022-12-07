783cc13627fae522aa02f9fca2e11e93743501ab
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-12-07 17:44:31 -0300

    salvando usuário no banco com o prisma

--- src/modules/user/useCases/CreateUserUseCase.ts
+++ src/modules/user/useCases/CreateUserUseCase.ts
@@ -1,3 +1,20 @@
-class CreateUserUseCase {}
+import { prisma } from "../../../database/prismaClient";
+interface IcreateUser {
+  username: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserUseCase {
+  async execute({ username, email, password }: IcreateUser) {
+    const user = await prisma.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: password,
+      },
+    });
+  }
+}
 
 export { CreateUserUseCase };
